4ba983f7b550080f36c0ccaa94930c1704d5f541
Christopher Richards <usa.chrisrichards@gmail.com>
2019-06-20 00:34:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,9 @@ def key_for_min_value(name_hash)
   lowest_value = nil
 
     name_hash.each do |key, value|
-      if key_for_min == nil
+      if name_hash == {}
+        key_for_min == nil
+        lowest_value == nil
       elsif value < lowest_value
         lowest_value = value
         key_for_min = key
